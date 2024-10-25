d37319eb0a53545835e9005fec5230b79711ce63
Vicente Duch <vhduch@gmail.com>
2024-10-25 19:10:07 +0200

    lab done Vicente Duch

--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,7 +1,8 @@
-const ExponentTwo = () => (
+const ExponentTwo = (props) => (
+  
    <div className="exponent-counter-container">
-    <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
+    <p className="exponent-label">{props.count}²</p>
+    <p className="exponent-result">{props.count} * {props.count} = <span className="total">{props.count * props.count}</span></p>
   </div>
 );
 
